545f607a3fda347340623fa68abaf22fb334c9fb
WissonTher <szweglinski@gmail.com>
2025-08-26 21:09:31 +0200

    Added logic for winter version of conquered peaks and saddles.

--- js/geojson/tatry-ponad-wszystko.js
+++ js/geojson/tatry-ponad-wszystko.js
@@ -1,54 +1,60 @@
 fetch('data/tatry-ponad-wszystko.geojson?v=' + Date.now())
     .then(response => response.json())
     .then(data => {
-                
         var tatrapeakGeojson = L.geoJSON(data, {
             pointToLayer: function(feature, latlng) {
                 var props = feature.properties;
-                let icon;
-                if (props.zdobyty) {
-                    if (props.rodzaj === "szczyt") {
-                        icon = greenIconSzczyt;
-                    } else {
-                        icon = greenIconPrzelecz;
-                    }
-                } else {
-                    if (props.rodzaj === "szczyt") {
-                        icon = redIconSzczyt;
-                    } else {
-                        icon = redIconPrzelecz;
-                    }
-                }
 
-                var marker = L.marker(latlng, { icon: icon, pane: props.zdobyty ? 'zdobytePane' : 'niezdobytePane' });
+                let summerIcon = props.zdobyty
+                    ? (props.rodzaj === "szczyt" ? greenIconSzczyt : greenIconPrzelecz)
+                    : (props.rodzaj === "szczyt" ? redIconSzczyt : redIconPrzelecz);
 
-                if (props.zdobyty) {
-                    zdobyteTatraLayer.addLayer(marker);
-                } else {
-                    niezdobyteTatraLayer.addLayer(marker);
-                }
-                allMarkers.addLayer(marker);
-
-                return marker;
-            },
-            onEachFeature: function(feature, layer) {
-                var props = feature.properties;
-                var iconUrl = layer.options.icon.options.iconUrl;
-                var popupContent = `
+                let summerPopup = `
                     <div style="display: flex; align-items: center;">
-                        <img src="${iconUrl}" style="width:100px;height:100px;margin-right:15px;">
+                        <img src="${summerIcon.options.iconUrl}" style="width:100px;height:100px;margin-right:15px;">
                         <div>
                             <b>${props.nazwa}</b><br>
                             Wysokość: ${props.wysokosc} m n.p.m.<br>
-                            ${props.zdobyty ? `Status: <b>Zdobyty</b><br>Data: ${props.data_zdobycia}<br>` +
-                            (props.instagram ? `<a href="${props.instagram}" target="_blank">Instagram</a>` : "")
-                            : `Status: <b>Nie zdobyty</b>`}
+                            ${props.zdobyty 
+                                ? `Status: <b>Zdobyty</b><br>Data: ${props.data_zdobycia || props.data}<br>` +
+                                (props.instagram ? `<a href="${props.instagram}" target="_blank">Instagram</a>` : "")
+                                : `Status: <b>Nie zdobyty</b>`}
                         </div>
                     </div>
                 `;
-                layer.bindPopup(popupContent);
+
+                let winterIcon = props.zima
+                    ? (props.rodzaj === "szczyt" ? blueIconSzczyt : blueIconPrzelecz)
+                    : (props.rodzaj === "szczyt" ? greyIconSzczyt : greyIconPrzelecz);
+
+                let winterPopup = `
+                    <div style="display: flex; align-items: center;">
+                        <img src="${winterIcon.options.iconUrl}" style="width:100px;height:100px;margin-right:15px;">
+                        <div>
+                            <b>${props.nazwa}</b><br>
+                            Wysokość: ${props.wysokosc} m n.p.m.<br>
+                            ${props.zima 
+                                ? `Status: <b>Zdobyty zimą</b><br>Data: ${props.data_zima}<br>` +
+                                (props.instagram ? `<a href="${props.instagram}" target="_blank">Instagram</a>` : "")
+                                : `Status: <b>Nie zdobyty zimą</b>`}
+                        </div>
+                    </div>
+                `;
+
+                var marker = L.marker(latlng, { icon: summerIcon, pane: props.zdobyty ? 'zdobytePane' : 'niezdobytePane' });
+
+                marker.options.summer = { icon: summerIcon, popup: summerPopup };
+                marker.options.winter = { icon: winterIcon, popup: winterPopup };
+                marker.bindPopup(summerPopup);
+
+                marker.options.summerStatus = props.zdobyty;
+                marker.options.winterStatus = props.zima;
+
+                allMarkers.addLayer(marker);
+
+                return marker;
             }
-        })
-        //updateOverlayLabels();
-    })
-    .catch(error => console.error("Błąd podczas wczytywania GeoJSON:", error));
+        });
+
+        updateMode('summer');
+    });
