358e033996e63cc1fe6661a213aaf3be157fecc8
WissonTher <szweglinski@gmail.com>
2025-08-31 19:37:19 +0200

    KGP: Added icons to popups.

--- js/geojson/korona-gor-polski.js
+++ js/geojson/korona-gor-polski.js
@@ -15,13 +15,22 @@ fetch('data/korona-gor-polski.geojson?v=' + Date.now())
                 return marker;
             },
             onEachFeature: function(feature, layer) {
+                var iconUrl = feature.properties.zdobyty ? greenIcon.options.iconUrl : redIcon.options.iconUrl;
                 var props = feature.properties;
-                var popupContent = `<b>${props.nazwa}</b><br>` +
-                    `Wysokość: ${props.wysokosc} m n.p.m.<br>` +
-                    `Pasmo: ${props.pasmo}<br>` +
-                    (props.zdobyty ? `Status: <b>Zdobyty</b><br>Data: ${props.data_zdobycia}<br>` +
-                    (props.instagram ? `<a href="${props.instagram}" target="_blank">Instagram</a>` : "") :
-                    `Status: <b>Nie zdobyty</b>`);
+                var popupContent = `
+                    <div style="display: flex; align-items: center;">
+                        <img src="${iconUrl}" style="width:75px;height:75px;margin-right:15px;">
+                        <div>
+                            <b>${props.nazwa}</b><br>
+                            Wysokość: ${props.wysokosc} m n.p.m.<br>
+                            Pasmo: ${props.pasmo}<br>
+                                ${props.zdobyty
+                                    ? `Status: <b>Zdobyty</b><br>Data: ${props.data_zdobycia}<br>
+                                        ${props.instagram ? `<a href="${props.instagram}" target="_blank">Instagram</a>` : ""}` 
+                                        : `Status: <b>Nie zdobyty</b>`}
+                        </div>
+                    </div>
+                `;
                 layer.bindPopup(popupContent);
             }
         });
