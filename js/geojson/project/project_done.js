bbad8b7760710b3fb31662e6a5ba1ca12117bf64
WissonTher <szweglinski@gmail.com>
2026-02-12 11:32:12 +0100

    Added 'done' parts of the new project layer.

--- js/geojson/project/project_done.js
+++ js/geojson/project/project_done.js
@@ -1,10 +1,18 @@
 fetch('data/project_done.geojson')
     .then(response => response.json())
     .then(data => {
+        var projectDoneWhite = L.geoJSON(data, {
+            pane: 'projectDonePane',
+            style: function(feature) {
+                return { color: "#300035", weight: 8, opacity: 0.9 };
+            },
+            interactive: false
+        }).addTo(projectDoneLayer);
+
         var projectDoneGeojson = L.geoJSON(data, {
             pane: 'projectDonePane',
             style: function(feature) {
-                return { color: "#73c53e", weight: 4, zIndex: 1};
+                return { color: "#ee49f4", weight: 4, zIndex: 1};
             },
             onEachFeature: function(feature, layer) {
                 var props = feature.properties;
@@ -12,6 +20,27 @@ fetch('data/project_done.geojson')
                 `Data przejścia: ${props.date}<br>` +
                 `<a href="${props.instagram}" target="_blank">Instagram</a>`
                 layer.bindPopup(popupContent);
+
+                var coords = feature.geometry.coordinates;
+                [coords[0], coords[coords.length - 1]].forEach(c => {
+                    L.circleMarker([c[1], c[0]], {
+                        radius: 6,
+                        color: "#000000",
+                        fillOpacity: 1,
+                        interactive: false,
+                        pane: 'vertexPane'
+                    }).addTo(projectDoneLayer);
+
+                    L.circleMarker([c[1], c[0]], {
+                        radius: 5,
+                        fillColor: "#447523",
+                        color: "#ffffff",
+                        weight: 3,
+                        fillOpacity: 1,
+                        interactive: false,
+                        pane: 'vertexPane'
+                    }).addTo(projectDoneLayer);
+                });
             }                    
         }).addTo(projectDoneLayer);
     })
